037958fd66b9d141252eb2f28e6359947bdd90ba
michouse <michouse@tree.odn.ne.jp>
2015-08-07 16:42:42 +0900

    各記事にソーシャルボタンを追加。

--- wp-content/themes/wordfes2015/template-parts/content-single.php
+++ wp-content/themes/wordfes2015/template-parts/content-single.php
@@ -13,12 +13,14 @@
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 			<div class="entry-meta clearfix">
+				<?php get_template_part( 'template-parts/social', 'single' ); ?>
 				<?php wordfes2015_posted_on(); ?>
 			</div><!-- .entry-meta -->
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
 			<?php the_content(); ?>
+			<?php get_template_part( 'template-parts/social', 'single' ); ?>
 			<?php
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wordfes2015' ),
---
+++ wp-content/themes/wordfes2015/template-parts/social-single.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * social.php
+ *
+ * @package WordFes Nagoya 2015
+ */
+?>
+<!--
+<?php var_dump( get_the_permalink() ); ?>
+-->
+<div class="social-area" style="margin: 15px 0 10px;">
+	<div class="sns-button fb-like" data-href="<?php echo get_the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+
+	<div class="sns-button twitter">
+	<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo get_the_permalink(); ?>" data-via="wbnagoya" data-lang="ja">ツイート</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
+</div><!-- .social-area -->
